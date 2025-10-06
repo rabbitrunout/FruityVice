385667062d85dd5edfd2b50a690a846b74dea975
Irina Saf <irina.safronova0801@gmail.com>
2025-10-06 08:15:56 -0400

    Initial Commit

---
+++ FruityVice/FruityViceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruityViceApp.swift
+//  FruityVice
+//
+//  Created by Irina Saf on 2025-10-06.
+//
+
+import SwiftUI
+
+@main
+struct FruityViceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
